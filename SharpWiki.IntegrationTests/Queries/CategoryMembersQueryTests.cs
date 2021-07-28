c7d590b21344f51f1384f4ae86522493e2084168
Misza <misza@misza.net>
2021-07-29 00:23:56 +0200

    Test category members query

---
+++ SharpWiki.IntegrationTests/Queries/CategoryMembersQueryTests.cs
@@ -0,0 +1,40 @@
+﻿namespace SharpWiki.IntegrationTests.Queries
+{
+    using System;
+    using System.Threading.Tasks;
+    using API.Queries;
+    using FluentAssertions;
+    using Infrastructure;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CategoryMembersQueryTests
+    {
+        private RestSharpApiWrapper wrapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            var apiUrl = new Uri("https://en.wikipedia.org/w/api.php");
+            this.wrapper = new RestSharpApiWrapper(apiUrl);
+        }
+        
+        [Test]
+        public async Task ShouldGetOnePage()
+        {
+            // Arrange
+            var queryParameters = new CategoryMembersQueryRequest("Category:Physics");
+
+            // Act
+            var result = await this.wrapper.Get(queryParameters);
+
+            // Assert
+            result.batchcomplete.Should().NotBeNull();
+            result.batchcomplete.Should().BeEmpty();
+
+            result.@continue.Should().BeNull();
+
+            result.query.categorymembers.Should().NotBeEmpty();
+        }
+    }
+}
